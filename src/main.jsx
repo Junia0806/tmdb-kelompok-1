0c3480446c3a3e7567ef0de31da0446725fb1d80
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-25 21:11:47 +0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NowPlaying from './andika-branch/movie.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
